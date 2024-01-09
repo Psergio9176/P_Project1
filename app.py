142b3da7e1c19cfa3cd0a48c35329e7317754186
Paulo_Santos <38445891+Psergio9176@users.noreply.github.com>
2024-01-08 23:37:35 -0300

    projeto de python alura opo

---
+++ app.py
@@ -0,0 +1,11 @@
+print('𝙎𝙖𝙗𝙤𝙧 𝙀𝙭𝙥𝙧𝙚𝙨𝙨\n')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print(f'Você escolheu a opção {opcao_escolhida}') 
+#Essa for usando o F para interpolar a string com o texto é a melhor forma segundo as boas praticas
+#Tem uma outra forma, mas é fechando a aspa e colocando o nome da variavel
