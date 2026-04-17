450aa35a8bea926ce7c409de3d289bfffaa48173
Paulo_Santos <38445891+Psergio9176@users.noreply.github.com>
2026-04-17 14:18:33 -0300

    fix: corrige erro TypeScript no deploy Vercel

--- ponto-frontend/src/sw/custom-sw.ts
+++ ponto-frontend/src/sw/custom-sw.ts
@@ -2,7 +2,7 @@ import { precacheAndRoute } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { NetworkFirst } from 'workbox-strategies';
 
-declare const self: ServiceWorkerGlobalScope;
+declare const self: any;
 
 precacheAndRoute(self.__WB_MANIFEST);
 
